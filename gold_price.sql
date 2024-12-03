209edd88e9d5286c7ef969d8464b21dac8363bde
Dybesto <limyijie1031@gmail.com>
2024-12-03 15:16:00 +0800

    seperated table for each currency

--- gold_price.sql
+++ gold_price.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2024 at 10:57 AM
+-- Generation Time: Dec 03, 2024 at 08:12 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,22 +18,189 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `nhihi`
+-- Database: `gold_price`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gold_price`
+-- Table structure for table `aud_price`
 --
 
-CREATE TABLE `gold_price` (
+CREATE TABLE `aud_price` (
   `PriceID` int(11) NOT NULL,
   `Price` float NOT NULL,
   `Date` date NOT NULL,
-  `Time` time NOT NULL,
-  `PriceChange(%)` float NOT NULL,
-  `Currency` varchar(3) NOT NULL
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brl_price`
+--
+
+CREATE TABLE `brl_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cad_price`
+--
+
+CREATE TABLE `cad_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chf_price`
+--
+
+CREATE TABLE `chf_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cny_price`
+--
+
+CREATE TABLE `cny_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eur_price`
+--
+
+CREATE TABLE `eur_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gbp_price`
+--
+
+CREATE TABLE `gbp_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hkd_price`
+--
+
+CREATE TABLE `hkd_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inr_price`
+--
+
+CREATE TABLE `inr_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jpy_price`
+--
+
+CREATE TABLE `jpy_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mxn_price`
+--
+
+CREATE TABLE `mxn_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rub_price`
+--
+
+CREATE TABLE `rub_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usd_price`
+--
+
+CREATE TABLE `usd_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zar_price`
+--
+
+CREATE TABLE `zar_price` (
+  `PriceID` int(11) NOT NULL,
+  `Price` float NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -41,9 +208,9 @@ CREATE TABLE `gold_price` (
 --
 
 --
--- Indexes for table `gold_price`
+-- Indexes for table `usd_price`
 --
-ALTER TABLE `gold_price`
+ALTER TABLE `usd_price`
   ADD PRIMARY KEY (`PriceID`);
 
 --
@@ -51,9 +218,9 @@ ALTER TABLE `gold_price`
 --
 
 --
--- AUTO_INCREMENT for table `gold_price`
+-- AUTO_INCREMENT for table `usd_price`
 --
-ALTER TABLE `gold_price`
+ALTER TABLE `usd_price`
   MODIFY `PriceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
