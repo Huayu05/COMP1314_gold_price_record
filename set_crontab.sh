1552f8ec17d13e3f0b6f383d2fd37182b6ef8743
Dybesto <limyijie1031@gmail.com>
2024-12-04 12:29:26 +0800

    optimized the path in configuring crontab for different users

--- set_crontab.sh
+++ set_crontab.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#redirect the output to the path based on different user
+path=$(readlink -f .)
+
 #Initialize the task needed to add
-TASK="0 * * * * /home/onghuayu/Git_Repo/COMP1314_CW2/insert_db.sh >> /home/onghuayu/Git_Repo/COMP1314_CW2/crontab_log.txt"
+TASK="* * * * * $path/insert_db.sh >> $path/crontab_log.txt"
 
 #Find the same task exist or not, if exist return 0 and ended
 (crontab -l | grep -Fxq "$TASK") || {
