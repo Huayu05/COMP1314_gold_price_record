d223c93c950d7aaa31859a5ece5741c383e6ba5d
Dybesto <limyijie1031@gmail.com>
2024-12-05 09:50:14 +0800

    Added script to draw graph based on specified date and time, updated readme.txt about the graph script

---
+++ gnu_plot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#Variables to define graph range
+start_date=$1
+end_date=$2
+start_hour=$3
+end_hour=$4
+currency=$5
+
+#fetch data to a file in the following format column1: Data column2: Price, column3: Time
+graph_data=$(sudo mysql -u root gold_price -e "SELECT Date, Price, Time FROM ${currency}_price WHERE (date BETWEEN '${start_date}' AND '${end_date}');" | sed '1d')
+
+#check if sql query faces error, (incorrect argument put into the script)
+#fetch the data from the query to a .dat filie
+if [ $? -ne 0 ]; then
+	echo "sql query error"
+	exit 1
+else
+	echo "$graph_data" > graph_data.dat
+fi
+
+#filter the data, the data will be within the specified date and hour
+awk -v end_date="$end_date" -v end_hour="$end_hour" -v start_date="$start_date" -v start_hour="$start_hour" '(($1 != end_date || $3 <= end_hour) && ($1 != start_date || $3 >= start_hour)){print $1 " " $3, $2}' graph_data.dat > tmp.dat && mv tmp.dat graph_data.dat
+
+#determine the range of the y axis to optimize the graph (find min and max price)
+maxprice=$(cat graph_data.dat | awk '{print $3}' | sort -n | tail -n 1 | awk '{print $1 + 10}')
+minprice=$(cat graph_data.dat | awk '{print $3}' | sort -n | head -n 1 | awk '{print $1 - 10}')
+
+#determine the ideal range for xaxis (date time)
+#calculate the differences in minutes and split them across 12 label
+time_diff=$(($(date -d "$end_date $end_hour" +%s) - $(date -d "$start_date $start_hour" +%s)))
+time_increment=$((time_diff/(60*12)))
+
+#graph configuration. Graph is created as graph.png
+#increment setting, format axis in time, specified range, specified x and y elements, graph labels, title and size
+gnuplot <<EOF
+set terminal png font 'Times new roman'
+set terminal png size 1200,800
+set output 'graph.png'
+set xlabel 'Date'
+set ylabel 'Price ${currency}'
+set xdata time 
+set xtics $time_increment minutes
+set timefmt "%Y-%m-%d %H:%M:%S"
+set format x "%Y-%m-%d\n%H:%M:%S"
+set xrange["${start_date} ${start_hour}":"${end_date} ${end_hour}"]	
+set yrange[${minprice}:${maxprice}]
+set title "Gold Price Chart"
+plot "graph_data.dat" u 1:3 w l t 'data'
+EOF
+
--- readme.txt
+++ readme.txt
@@ -1,5 +1,9 @@
 Required Execution:
 1. sudo ./set_contrab.sh - Configure setting for cronjob
 2. Import gold_price.sql into database server
-3. ./insert_db.sh to manually insert gold price informations
+
+Execution method
+1. ./insert_db.sh to manually insert gold price information
+2. Plot graph using the following script + arguments: ./gnu_plot.sh startdate enddate starttime endtime currency
+
 
